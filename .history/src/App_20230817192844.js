efb73394d1ee4f721884cb482b1dc701784036aa
Tracy-maker <ydlvns@gmail.com>
2023-08-17 19:32:31 +1000

    rewrite

---
+++ .history/src/App_20230817192844.js
@@ -0,0 +1,16 @@
+import { useContext, useEffect } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BooksContext from "./context/books";
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      
+    </div>
+  );
+}
+
+export default App;
--- src/hooks/use-books-context.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
-
-function useBookContext(){
-    return useContext(BooksContext);
-}
-export default useBookContext;
